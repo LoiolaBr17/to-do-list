c1184c789e8e2b8d036477dbd39554492de37269
LoiolaBr17 <mateustaua16@gmail.com>
2022-05-06 00:34:53 -0300

    todo list simples

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TodoList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import TodoList from './components/TodoList.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      TodoList
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  .container{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      background-color: #808080;
+      color: #fff;
+      min-height: 100vh;
+    }
+
+    .entrada {
+      width: 25%;
+      margin: 20px auto;
+      padding: 10px;
+    }
+
+    .lista{
+      width: 25%;
+      margin: 20px auto;
+      background-color: #000;
+    }
+
+    .lista button {
+      width: 20px;
+      float: right;
+    }
+
+    ul li {
+      list-style-type: none;
+      text-align: center;
+      margin-bottom: 10px;
+      padding: 10px;
+    } 
+
+    #inserir {
+      width: 70px;
+      padding: 10px;
+      margin: 0 auto;
+      cursor: pointer;
+    }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        
+        <input v-model="conteudoInput" type="text" class="entrada">
+        <button id="inserir" @click="inserir()">ADD</button>
+       
+        <ul class="lista">
+            <li v-for="(nota, index) in notas" :key="index"><span>{{nota.texto}}</span><button @click="apagar(index)">X</button></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TodoList',
+        data(){
+            return {
+                conteudoInput: '',
+                notas: [
+                    {texto: "teste de texto 1"},
+                ]
+            }
+        },
+        methods:{
+            apagar(index){
+                this.notas.splice(index,1)
+            },
+            inserir(){
+                console.log(this.conteudoInput)
+                if(this.conteudoInput === '') return
+                this.notas.push({texto: this.conteudoInput})
+
+                this.conteudoInput = ''
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+</style>
